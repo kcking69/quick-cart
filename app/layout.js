5047da75e4d0fd7695d955469a3cb2023dfb05a3
Nkemdilim Collins <collinsnkemdilim05@gmail.com>
2025-07-22 16:59:34 +0100

    more blueify changes

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,8 @@ import { ClerkProvider } from '@clerk/nextjs';
 const outfit = Outfit({ subsets: ['latin'], weight: ['300', '400', '500'] });
 
 export const metadata = {
-  title: 'QuickCart - GreatStack',
-  description: 'E-Commerce with Next.js ',
+  title: 'QuickCart - Collins',
+  description: 'E-Commerce Website ',
 };
 
 export default function RootLayout({ children }) {
